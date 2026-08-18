3d35885f5493ac86cc1573db28172c8b4927f183
Kazi Rifat Al Muin <muin2107042@stud.kuet.ac.bd>
2026-08-18 23:43:25 +0600

    Image Restoration

---
+++ 3 - Image Restoration/fourier_base.py
@@ -0,0 +1,53 @@
+# Fourier transform - guassian lowpass filter
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# take input
+img_input = cv2.imread('pnois2.jpg', 0)
+img = img_input.copy()
+image_size = img.shape[0] * img.shape[1]
+
+
+#%%
+# fourier transform
+ft = np.fft.fft2(img)
+ft_shift = np.fft.fftshift(ft)
+
+# get magnitude and phase
+magnitude_spectrum_ac = np.abs(ft_shift) #actual magnitude spectrum
+ang = np.angle(ft_shift)
+
+#Just for visualization
+magnitude_spectrum = 20 * np.log(np.abs(ft_shift)+1)
+magnitude_spectrum = cv2.normalize(magnitude_spectrum, None,0,255,cv2.NORM_MINMAX,dtype=cv2.CV_8U) 
+
+# phase visualization if needed
+ang_ = cv2.normalize(ang, None,0,255,cv2.NORM_MINMAX,dtype=cv2.CV_8U) 
+
+
+# Apply filter here: 
+# Design H(u,v) an ideal notch reject filter to remove periodic noise and just do 
+# F(u,v) = F(u,v)*H(u,v) to get the filtered result here F(u,v) is the  magnitude spectrum 
+magnitude_spectrum_ac[261,261] = 0
+magnitude_spectrum_ac[251,251] = 0
+
+
+## phase add F(u,v)=∣F(u,v)∣*e^jθ(u,v) combine magnitude and phase to get the final result
+final_result = np.multiply(magnitude_spectrum_ac, np.exp(1j*ang))
+
+# inverse fourier transform and get the image back
+img_back = np.real(np.fft.ifft2(np.fft.ifftshift(final_result)))
+img_back_scaled = cv2.normalize(img_back, None, 0,255,cv2.NORM_MINMAX,dtype=cv2.CV_8U)
+
+
+## plot
+cv2.imshow("input", img_input)
+cv2.imshow("Magnitude Spectrum",magnitude_spectrum)
+cv2.imshow("Phase", ang_)
+cv2.waitKey(0)
+cv2.imshow("Inverse transform",img_back_scaled)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
